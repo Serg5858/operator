ed0cd2ba8fa140c5092028fcbc3327c7f38feffa
Serg5858 <ss3@ro.ru>
2022-07-06 11:57:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccount = 100;
+        int additionOfAccount = 1000;
+        int bonus = additionOfAccount / 100;
+        int amount = currentAccount + additionOfAccount + bonus;
+        if (additionOfAccount >= 1000) {
+            System.out.println(" Итоговый счет :" + amount + " из них бонус :" + bonus);
+        } else {
+            System.out.println("Итоговый счет :" + (currentAccount + additionOfAccount));
+        }
+    }
+}
+
+
+
