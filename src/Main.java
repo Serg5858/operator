5b14c167f2a36f83f9a31fb2b896ece769af742d
Serg5858 <ss3@ro.ru>
2022-07-10 11:33:04 +0300

    added more than 1000

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int currentAccount = 100;
-        int additionOfAccount = 1000;
+        int additionOfAccount = 1100;
         int bonus = additionOfAccount / 100;
         int amount = currentAccount + additionOfAccount + bonus;
-        if (additionOfAccount >= 1000) {
+        if (additionOfAccount > 1000) {
             System.out.println(" Итоговый счет :" + amount + " из них бонус :" + bonus);
         } else {
             System.out.println("Итоговый счет :" + (currentAccount + additionOfAccount));
